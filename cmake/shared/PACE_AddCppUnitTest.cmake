256c73dbeb1d2ca86e7983bf501db0f3e7f1834e
rocky Cloud User <rocky@alex-buts-testing.novalocal>
2026-07-20 13:30:31 +0100

    Re #1954 script which accepts various link options to link against matlab libstdc++.so. Does not resolves stdc links properly

--- cmake/shared/PACE_AddCppUnitTest.cmake
+++ cmake/shared/PACE_AddCppUnitTest.cmake
@@ -62,18 +62,34 @@ function(pace_add_cpp_unit_test)
     # Create the test executable
     add_executable("${TEST_NAME}" "${TEST_SOURCES}")
     target_include_directories("${TEST_NAME}" PRIVATE "${CXX_SOURCE_DIR}")
-    target_link_libraries("${TEST_NAME}" PRIVATE gtest_main "${TEST_LIBRARIES}")
+    #target_link_libraries("${TEST_NAME}" PRIVATE gtest_main "${TEST_LIBRARIES}")
     set_target_properties("${TEST_NAME}" PROPERTIES
         FOLDER "/home/rocky/m_cm_test/utility/Tests"
         RUNTIME_OUTPUT_DIRECTORY "${TESTS_BIN_DIR}"
     )
     # If MEX_TEST flag was passed to function, link to Matlab libraries
     if("${TEST_MEX_TEST}")
-	target_link_libraries("${TEST_NAME}" PRIVATE
-		"${Matlab_LIBRARIES}"
-		)
-		#		"${Matlab_LIBRARIES};/opt/modules-common/software/MATLAB/R2024b/sys/os/glnxa64/libstdc++.so.6"
-		#	       )
+	    target_link_directories("${TEST_NAME}" PRIVATE
+		  "/home/rocky/bin/libstdcxx-10-for-matlab/lib64/"
+		  "/usr/lib64"
+	    )
+	    #target_link_libraries("${TEST_NAME}" PRIVATE
+	    #	   gtest_main	    
+	    #	   -Wl,--start-group
+	    #      "${TEST_LIBRARIES}"
+	    #      "${Matlab_LIBRARIES}"		   
+	    #	   /opt/modules-common/software/MATLAB/R2024a/sys/os/glnxa64/libstdc++.so.6
+	    #	   -Wl,--end-group
+	    #)
+	    target_link_libraries("${TEST_NAME}" PRIVATE
+ 		   gtest_main
+		   gtest
+		   ${Matlab_LIBRARIES}
+		   ${TEST_LIBRARIES}
+	     )
+	    #target_link_libraries("${TEST_NAME}" PRIVATE
+	    # 	"${Matlab_LIBRARIES};/opt/modules-common/software/MATLAB/R2024a/sys/os/glnxa64/libstdc++.so.6"
+	    #		 )
         target_include_directories(
             "${TEST_NAME}" PRIVATE "${Matlab_INCLUDE_DIRS}")
     endif()
@@ -104,14 +120,19 @@ function(pace_add_cpp_unit_test)
     else()
         MESSAGE("MATLAB LIBRARIES: ${Matlab_LIBRARIES}")
         MESSAGE("TEST NAME: ${_full_test_name}")        
-	#target_link_options("${TEST_NAME}" PRIVATE -nostdlib++)
-	#target_link_libraries("${TEST_NAME}"  PRIVATE 
-	#	"/opt/modules-common/software/MATLAB/R2024b/sys/os/glnxa64/libstdc++.so.6"
-	#)
-	#set_target_properties("${TEST_NAME}"
-	#    PROPERTIES
-	#    BUILD_RPATH "/opt/modules-common/software/MATLAB/R2024b/sys/os/glnxa64/"
-	#)
+	target_link_options("${TEST_NAME}" PRIVATE -nostdlib)
+        target_link_libraries("${TEST_NAME}"  PRIVATE 
+	   "/home/rocky/bin/libstdcxx-10-for-matlab/lib64/libstdc++.so"
+	   "/usr/lib64/libpthread.so"
+	   "/usr/lib64/libgcc_s.so.1"
+	   "/usr/lib64/libm.so.6"
+	   "/usr/lib64/libc.so.6"
+	)
+	set_target_properties("${TEST_NAME}"
+	    PROPERTIES
+	    BUILD_RPATH "/home/rocky/bin/libstdcxx-10-for-matlab/lib64/"
+	)
     endif()
 
 endfunction()
+
